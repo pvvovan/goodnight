7ba362c26a76c1f21c53456b570b01252d5c53b4
PVVovan <pvvovan@gmail.com>
2025-04-12 09:38:05 +0200

    Precompile iostream header

--- build.sh
+++ build.sh
@@ -2,16 +2,15 @@
 
 # Build application for Linux by cmake
 
-# Check cmake version
-#~/cmake-4.0.1-linux-x86_64/bin/cmake --version
-
-# Precomplie iostream module to prevent compilation errors
-#g++ -std=c++23 -fmodules-ts -xc++-system-header iostream
-
 SCRIPT_DIR=$(dirname "$(readlink -f $0)")
 DEBUG_DIR=${SCRIPT_DIR}/Debug
+
 if [ ! -d ${DEBUG_DIR} ]; then
 	mkdir ${DEBUG_DIR}
+	cd ${DEBUG_DIR}
+	# Precomplie iostream module to prevent compilation errors
+	g++ -std=c++23 -fmodules-ts -xc++-system-header iostream
 fi
 
-g++ Src/main.cpp --std=c++23 -fmodules-ts -o ./Debug/goodnight.elf
+cd ${DEBUG_DIR}
+g++ ${SCRIPT_DIR}/Src/main.cpp --std=c++23 -fmodules-ts -o ${DEBUG_DIR}/goodnight.elf
