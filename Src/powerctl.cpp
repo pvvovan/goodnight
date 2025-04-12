d912c4dc35057ee1de2456a6c8647a66b380f2bd
PVVovan <pvvovan@gmail.com>
2025-04-12 10:15:01 +0200

    Add support for PC poweroff

--- Src/powerctl.cpp
+++ Src/powerctl.cpp
@@ -1,10 +1,12 @@
 export module powerctl;
-
 import <iostream>;
+#include <sys/reboot.h>    /* Definition of RB_* constants */
+#include <unistd.h>
 
 namespace power {
 	export void off()
 	{
+		::reboot(RB_POWER_OFF);
 		std::cout << "Good night!!!" << std::endl;
 	}
 }
