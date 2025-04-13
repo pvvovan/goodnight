aeab522f9fb96b0aa32d0ecb88aac4c6db3f0f0a
PVVovan <pvvovan@gmail.com>
2025-04-13 11:01:25 +0200

    Create systemd service file

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # goodnight
 
 Daemon ensuring children go to bed in time
+
+Copy goodnight.service to /etc/systemd/system/goodnight.service
+Set path to goodnight executable in goodnight.service
+sudo systemctl deamon-reload
+sudo systemctl enable goodnight.service
+sudo systemctl start goodnight.service
+
--- Src/goodnight.cpp
+++ Src/goodnight.cpp
@@ -11,7 +11,7 @@ namespace pc {
 				std::thread th { [] () -> void {
 						ntp::client ntp_cl{};
 						for ( ; ; ) {
-							std::this_thread::sleep_for(std::chrono::seconds(5));
+							std::this_thread::sleep_for(std::chrono::minutes(5));
 							const int hh = mclock::hours(ntp_cl.get_epoch());
 							if (hh >= 23) {
 								power::off();
