9f6b6039a84aa5eed5a23255db193867701f0c94
PVVovan <pvvovan@gmail.com>
2025-12-03 21:20:08 -0800

    Create timer

--- Win/GoodNight/GoodNightService.cs
+++ Win/GoodNight/GoodNightService.cs
@@ -17,12 +17,24 @@ namespace GoodNight
             InitializeComponent();
         }
 
+        private System.Threading.Timer timer;
+
         protected override void OnStart(string[] args)
         {
+            timer = new System.Threading.Timer(OnTimerCallback);
+            timer.Change(60_000, 300_000);
         }
 
         protected override void OnStop()
         {
+            timer.Change(System.Threading.Timeout.Infinite, System.Threading.Timeout.Infinite);
+            timer.Dispose();
+            timer = null;
+        }
+
+        private void OnTimerCallback(object state)
+        {
+
         }
     }
 }
