08ada45b155e089bc86de241393b9130d4aa9fd7
PVVovan <pvvovan@gmail.com>
2025-12-04 21:19:39 -0800

    Uninstall.ps1

--- Win/Uninstall.ps1
+++ Win/Uninstall.ps1
@@ -1,2 +1,2 @@
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\InstallUtil.exe `
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\InstallUtil.exe /u `
         $PSScriptRoot\GoodNight\bin\Debug\GoodNight.exe
